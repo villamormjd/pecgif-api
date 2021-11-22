6a68d704b4eaf26fc7f9f34321ac8823deabe1f7
villamormjd <sdjango@coretechis.com>
2021-11-22 16:53:06 +0800

    Updated databases

--- pecgif/settings.py
+++ pecgif/settings.py
@@ -80,9 +80,15 @@ WSGI_APPLICATION = 'pecgif.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config("DATABASE_ENGINE"),
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
     }
 }
 
