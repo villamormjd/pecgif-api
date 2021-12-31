05cb30e5c948f527c1caf85d2f503c5a4c050d85
villamormjd <sdjango@coretechis.com>
2021-12-31 11:02:11 +0800

    Enhancement

--- portfolio/portfolioApi.py
+++ portfolio/portfolioApi.py
@@ -45,12 +45,11 @@ class PortfolioListView(APIView):
         :return:
         '''
 
-        portfolios = Portfolio.objects.all().order_by('-created_on')
+        portfolios = Portfolio.objects.all().order_by('created_on')
         serializers = PortfolioSerializers(portfolios,many=True, context={"request": request})
-        print(portfolios[0].buying_power, portfolios[1].buying_power)
-        previous = portfolios[0]
-        current = portfolios[1]
-        print(previous, current)
+        print(list(portfolios)[-1], list(portfolios)[-2])
+        previous = list(portfolios)[-2]
+        current = list(portfolios)[-1]
         pct_nlv = get_pct_change(previous.nlv, current.nlv)
         #print("NLV",pct_nlv)
         pct_buying_power = get_pct_change(previous.buying_power, current.buying_power)
@@ -58,7 +57,7 @@ class PortfolioListView(APIView):
         pct_total_positions = get_pct_change(previous.total_positions, current.total_positions)
        # print("POS", pct_total_positions)
         return Response({"error": False, "message": "Portfolio retrieved.",
-                         "data": serializers.data[0],
+                         "data": serializers.data[-1],
                          "nlv_pct": pct_nlv,
                          "pwr_pct": pct_buying_power,
                          "pos_pct": pct_total_positions,
